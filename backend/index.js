d73c3df23aaf1f4af9fdc546205d8db69d7db680
finik7123 <kapitanov.2006@mail.ru>
2025-05-14 23:51:28 +0200

    productINFO

--- backend/index.js
+++ backend/index.js
@@ -11,33 +11,34 @@ const pool = new Pool({
     database: 'postgres',
     password: '1234',
     port: 5432,
+    options: '-c search_path=wine'
 });
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.get('/api/wines', async (req, res) => {
-  try {
-      const { rows } = await pool.query('SELECT * FROM wine_assortment');
-      res.json(rows);
-  } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-  }
+    try {
+        const { rows } = await pool.query('SELECT * FROM wine_assortment');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.get('/api/wines/:id', async (req, res) => {
-  const { id } = req.params;
-  try {
-      const { rows } = await pool.query('SELECT * FROM wine_assortment WHERE id = $1', [id]);
-      if (rows.length === 0) {
-          return res.status(404).json({ error: 'Wine not found' });
-      }
-      res.json(rows[0]);
-  } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: 'Internal server error' });
-  }
+    const { id } = req.params;
+    try {
+        const { rows } = await pool.query('SELECT * FROM wine_assortment WHERE id = $1', [id]);
+        if (rows.length === 0) {
+            return res.status(404).json({ error: 'Wine not found' });
+        }
+        res.json(rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.get('/api/whiskey', async (req, res) => {
@@ -48,9 +49,9 @@ app.get('/api/whiskey', async (req, res) => {
         console.error(err);
         res.status(500).json({ error: 'Internal server error' });
     }
-  });
-  
-  app.get('/api/whiskey/:id', async (req, res) => {
+});
+
+app.get('/api/whiskey/:id', async (req, res) => {
     const { id } = req.params;
     try {
         const { rows } = await pool.query('SELECT * FROM whiskey_assortment WHERE id = $1', [id]);
@@ -62,7 +63,7 @@ app.get('/api/whiskey', async (req, res) => {
         console.error(err);
         res.status(500).json({ error: 'Internal server error' });
     }
-  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server starting on port ${PORT}`);
